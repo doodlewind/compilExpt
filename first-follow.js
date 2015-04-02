6372adf27502a20dab946bb97f2dea9659a735b4
ewind <ewind@mail.ustc.edu.cn>
2015-04-02 14:56:42 +0800

    add FIRST set builder
    Dynamically create FIRST set according to grammar, also refactored data
    structure of grammar rules.

--- first-follow.js
+++ first-follow.js
@@ -1,174 +1,178 @@
-String.prototype.type = function () {
-    var token = this;
-    var type = token.match(/^(\(|\)|\*)|([a-z]+)/);
-    if (type !== null) {
-        return type[0];
-    }else {
-        return '';
-    }
-};
-
-String.prototype.type = function () {
-    var token = this;
-    return token[0];
-};
-
 function hasValue(arr,obj) {
     return (arr.indexOf(obj) != -1);
 }
 
-var clone = function(obj){
-    var newObj = obj.constructor === Array ? [] : {};
-    if(typeof obj !== 'object'){
-        return;
-    } else {
-        for(var i in obj){
-            newObj[i] = typeof obj[i] === 'object' ?
-                clone(obj[i]) : obj[i];
-        }
+// symbols list
+var S = {
+    'E': {
+        type: 'NonTerminal',
+        str: 'E'
+    },
+    'E_': {
+        type: 'NonTerminal',
+        str: 'E_'
+    },
+    'T': {
+        type: 'NonTerminal',
+        str: 'T'
+    },
+    'T_': {
+        type: 'NonTerminal',
+        str: 'T_'
+    },
+    'F': {
+        type: 'NonTerminal',
+        str: 'F'
+    },
+    'id': {
+        type: 'Terminal',
+        str: 'id'
+    },
+    '*': {
+        type: 'Terminal',
+        str: '*'
+    },
+    '+': {
+        type: 'Terminal',
+        str: '+'
+    },
+    '(': {
+        type: 'Terminal',
+        str: '('
+    },
+    ')': {
+        type: 'Terminal',
+        str: ')'
+    },
+    '': {
+        type: 'Terminal',
+        str: ''
     }
-    return newObj;
-};
-
-var Dot = {
-    str: "●",
-    type: function() {return Dot},
-    toString: function () {return "●"}
-};
-var E = {
-    str: "E",
-    type: function() {return E},
-    toString: function () {return "E"}
-};
-var T = {
-    str: "T",
-    type: function() {return T},
-    toString: function () {return "T"}
-};
-var T_ = {
-    str: "T_",
-    type: function() {return T_},
-    toString: function () {return "T_"}
-};
-var E_ = {
-    str: "E_",
-    type: function() {return E_},
-    toString: function () {return "E_"}
-};
-var F = {
-    str: "F",
-    type: function() {return F},
-    toString: function () {return "F"}
-};
-var id = {
-    str: "id",
-    type: function() {return id},
-    toString: function () {return "id"}
 };
 
 
+var r0 = {'from': S['E'], 'to': [S['T'], S['E_']]};
+var r1 = {'from': S['E_'], 'to': [S['+'], S['T'], S['E_']]};
+var r2 = {'from': S['E_'], 'to': [S['']]};
+var r3 = {'from': S['T'], 'to': [S['F'], S['T_']]};
+var r4 = {'from': S['T_'], 'to': [S['*'], S['F'], S['T_']]};
+var r5 = {'from': S['T_'], 'to': [S['']]};
+var r6 = {'from': S['F'], 'to': [S['('], S['E'], S[')']]};
+var r7 = {'from': S['F'], 'to': [S['id']]};
+var GRAMMAR = [r0, r1, r2, r3, r4, r5, r6, r7];
 
-var r0 = {'from': E, 'to': [T, E_]};
-var r1 = {'from': E_, 'to': ['+', T, E_]};
-var r2 = {'from': E_, 'to': ['']};
-var r3 = {'from': T, 'to': [F, T_]};
-var r4 = {'from': T_, 'to': ['*', F, T_]};
-var r5 = {'from': T_, 'to': ['']};
-var r6 = {'from': F, 'to': ['(', E, ')']};
-var r7 = {'from': F, 'to': ['']};
-var grammar = [r0, r1, r2, r3, r4, r5, r6, r7];
-var symbols = [E, E_, T, T_, F, '(', ')', '*', 'id', ''];
+var FIRST = {'F': [], 'T': [], 'E': [], 'E_': [], 'T_': [], '(': [], ')': [], '*': [], id: [], '+': [], '': []};
+var FOLLOW = {'F': [], 'T': [], 'E': [], 'E_': [], 'T_': [], '(': [], ')': [], '*': [], id: [], '+': [], '': []};
 
+//correct FIRST set
 //var FIRST = {
-//    F: [],
-//    T: [],
-//    E: [],
-//    E_: [],
-//    T_: [],
-//    '(': [],
-//    ')': [],
-//    '*': [],
-//    'id': [],
-//    '': []
+//    'F': [S['('], S['id']],
+//    'T': [S['('], S['id']],
+//    'E': [S['('], S['id']],
+//    'E_': [S['+'], S['']],
+//    //T_: ['*', ''],
+//    //F: [],
+//    //T: [],
+//    //E: [],
+//    //E_: [],
+//    'T_': [],
+//    '(': [S['(']],
+//    ')': [S[')']],
+//    '*': [S['*']],
+//    'id': [S['id']],
+//    '+': [S['+']],
+//    '': [S['']]
 //};
-var FIRST = {
-    F: ['(', 'id'],
-    T: ['(', 'id'],
-    E: ['(', 'id'],
-    E_: ['+', ''],
-    //T_: ['*', ''],
-    T_: [],
-    '(': ['('],
-    ')': [')'],
-    '*': [')'],
-    'id': ['id'],
-    '+': ['+'],
-    '': ['']
-};
 
-// input an symbol array
-function first(symbols) {
 
-    // find FIRST of single symbol
-    if (symbols.length === undefined || symbols.length == 1) {
-        var symbol = symbols.length === undefined ? symbols : symbols[0];
 
+// run for once, build FIRST set for all symbols
+function buildFirstTable() {
+    while (true) {
+        var changed = false;
+        for (var s in S) {
+            //console.log("calculate first: " + s);
+            if (buildFirst(S[s])) {
+                changed = true;
+            }
+        }
+        //console.log("\n");
+        if (changed == false) break;
+    }
+
+    // input an symbol S['X']
+    // modify its FIRST set according to current FIRST table
+    function buildFirst(symbol) {
+        var changed = false;
+
+        // FIRST of terminal is itself
         if (isTerminal(symbol)) {
-            FIRST[symbol] = [symbol];
-        } else {
-            for (var i in grammar) {
-                if (grammar[i]['from'] == symbol) {
+            //console.log("change symbol is Terminal");
+            if (!hasValue(FIRST[symbol.str], symbol)) {
+                changed = true;
+                FIRST[symbol.str] = [symbol];
+            }
+        }
+        // to Non-Terminal, apply rules
+        else {
+            for (var i in GRAMMAR) {
+                if (GRAMMAR[i]['from'] == symbol) {
                     // X → A B C...
-                    if (grammar[i]['to'] !== '') {
-                        var item = grammar[i];
+                    if (GRAMMAR[i]['to'] !== '') {
+                        var item = GRAMMAR[i];
 
                         // for all symbols in A → B C D E
                         for (var j = 0; j < item['to'].length; j++) {
+
                             var s = item['to'][j];
-                            if (!hasValue(FIRST[s], '')) {
+                            // S[''] stands for epsilon
+                            if (!hasValue(FIRST[s.str], S[''])) {
                                 break;
                             }
                         }
                         // each of item[0] to item[j] has ɛ in FIRST
                         // their FIRST symbol should be added
-                        for (var k = 0; k < j; k++) {
-                             addIntoFirst(symbol, item['to'][k]);
+                        if (j == 0) {
+                            addIntoFirst(symbol.str, item['to'][0].str);
+                        }
+                        else {
+                            for (var k = 0; k < j; k++) {
+                                addIntoFirst(symbol.str, item['to'][k].str);
+                            }
                         }
 
                     } else{
                         // X → ɛ
-                        FIRST[symbol].push('');
+                        // add in epsilon
+                        // console.log("change X to epsilon");
+                        if (!hasValue(FIRST[symbol.str])) {
+                            changed = true;
+                            FIRST[symbol.str].push(S['']);
+                        }
                     }
                 }
             }
         }
-    }
+        return changed;
 
-    function addIntoFirst(dest, src) {
-        for (var i = 0; i < FIRST[src].length; i++) {
-            if (!hasValue(FIRST[dest], FIRST[src][i])) {
-                FIRST[dest].push(FIRST[src][i]);
+        // dest and src are in 'X' format (not S['X'])
+        function addIntoFirst(dest, src) {
+            for (var i = 0; i < FIRST[src].length; i++) {
+                if (!hasValue(FIRST[dest], FIRST[src][i])) {
+                    changed = true;
+                    //console.log("change: addIntoFIRST");
+                    FIRST[dest].push(FIRST[src][i]);
+                }
             }
         }
-    }
 
-    function isTerminal(symbol) {
-        // terminal is not instance of Object
-        return !(symbol instanceof Object);
-    }
-
-    function showProduction(p) {
-        var tmp = "";
-        for (var i in p['to']) {
-            if (p['to'][i] === '') {
-                tmp += " ɛ ";
-            }
-            tmp += " " + p['to'][i].toString();
+        function isTerminal(symbol) {
+            return symbol.type === 'Terminal';
         }
-        return (p['from'].toString() + " →" + tmp);
     }
-
 }
 
-first([T_]);
-console.log(FIRST);
+
+
+buildFirstTable();
+//console.log(FIRST['T']);
